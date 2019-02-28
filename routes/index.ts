8551ccac18594abdf5d0d95980d077b13820228c
dengshuaiyuan <18716027109@163.com>
2019-02-28 17:38:41 +0800

    vscode debug

--- routes/index.ts
+++ routes/index.ts
@@ -10,7 +10,7 @@ router.get('/', async (ctx, next) => {
   /*await ctx.render('index', {
     title: 'Hello Koa 2!'
   })*/
-  ctx.body = 'connected'
+  ctx.body = 'connected alll'
 })
 
 router.get('/string', async (ctx, next) => {
